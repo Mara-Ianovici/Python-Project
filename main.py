a5de0e48f5d1d5624b9c3f62097e209f1010b464
Mara-Ianovici <maraian0905@gmail.com>
2022-12-08 14:33:01 +0200

    Added: - error handling for the request - recursive page find - logging status, page link and time to complete the operations - printed final tree structure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import copy
-
+import time
 import requests
 import re
 from urllib.parse import urlparse
@@ -35,14 +35,45 @@ def spider(starting_page, tag):
         return
 
     visited_pages.append(starting_page)
-    response = requests.get(starting_page)
-    print(f"[{response.status_code}] - {starting_page}")
-    response_text = response.text
-    hrefs_list = get_matched_hrefs(starting_page, tag, response_text)
-    print(hrefs_list)
-    spider_tree[starting_page] = hrefs_list[1:]
-    spider_tree[starting_page].append(len(response_text))
+    try:
+        response = requests.get(starting_page)
+        print(f"[{response.status_code}] - {starting_page}")
+        response_text = response.text
+        hrefs_list = get_matched_hrefs(starting_page, tag, response_text)
+        spider_tree[starting_page] = hrefs_list[1:]
+        spider_tree[starting_page].append(len(response_text))
+
+        for h in hrefs_list:
+            if h not in visited_pages:
+                spider(h, tag)
+    except requests.exceptions.RequestException as e:
+        print(f"[ERROR] {e}")
+        spider_tree[starting_page] = ["[ERROR]"]
+
+
+def print_spider_tree(current_key, indent, parents):
+    global spider_tree
+    indent_tmp = indent
+    indent_str = ""
+    while indent_tmp:
+        indent_str += '----'
+        indent_tmp -= 1
+    parents_tmp = copy.deepcopy(parents)
+    parents_tmp.append(current_key)
+    print(f"{indent_str}{current_key} -> {spider_tree[current_key][-1]}")
+    for item in spider_tree[current_key][:-1]:
+        if len(parents) > 0 and item in parents:
+            print(f"{indent_str}----{item} -> {spider_tree[item][-1]} (...infinite loop...)")
+        else:
+            print_spider_tree(item, indent + 1, parents_tmp)
 
 
 if __name__ == '__main__':
+    time_n = time.time()
     spider('https://profs.info.uaic.ro/~ciortuz/', "ciortuz")
+    time_spider = time.time() - time_n
+    time_n = time.time()
+    print_spider_tree('https://profs.info.uaic.ro/~ciortuz/', 0, [])
+    time_print = time.time() - time_n
+    print(f"Total time to get all links: {time_spider}")
+    print(f"Total time to get the spider structure: {time_print}")
